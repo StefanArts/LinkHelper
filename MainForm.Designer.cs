3cd9e9f03b38833ebb88c65a5c179ec89d1f02f3
Stefan H <stefanarts@gmx.net>
2020-08-03 02:14:00 +0200

    Version 1.0-snapshot

---
+++ MainForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace LinkHelper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            ChangeClipboardChain(this.Handle, nextClipboardViewer);
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.versionLabel = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.shortSize = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.shortSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.trayIcon.BalloonTipText = "LinkHelper läuft!";
+            this.trayIcon.BalloonTipTitle = "LinkHelper wurde in den Hintergrund verschoben!";
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "LinkHelper";
+            this.trayIcon.Visible = true;
+            this.trayIcon.DoubleClick += new System.EventHandler(this.onClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LinkHelper";
+            // 
+            // versionLabel
+            // 
+            this.versionLabel.AutoSize = true;
+            this.versionLabel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.versionLabel.Location = new System.Drawing.Point(11, 41);
+            this.versionLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.versionLabel.Name = "versionLabel";
+            this.versionLabel.Size = new System.Drawing.Size(78, 16);
+            this.versionLabel.TabIndex = 1;
+            this.versionLabel.Text = "Version: %s";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(11, 130);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(165, 16);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "by StefanArts Development";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Terminate LinkHelper";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Link Host: s-ls.de";
+            // 
+            // shortSize
+            // 
+            this.shortSize.Location = new System.Drawing.Point(107, 60);
+            this.shortSize.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.shortSize.Minimum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.shortSize.Name = "shortSize";
+            this.shortSize.Size = new System.Drawing.Size(63, 22);
+            this.shortSize.TabIndex = 5;
+            this.shortSize.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.shortSize.ValueChanged += new System.EventHandler(this.shortSize_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Shorting Size:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(180, 159);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.shortSize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.versionLabel);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LinkHelper";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.onClose);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.shortSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label versionLabel;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown shortSize;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinkHelper
+{
+
+    public partial class MainForm : Form
+    {
+        [DllImport("User32.dll")]
+        protected static extern int
+            SetClipboardViewer(int hWndNewViewer);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool
+               ChangeClipboardChain(IntPtr hWndRemove,
+                                    IntPtr hWndNewNext);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg,
+                                             IntPtr wParam,
+                                             IntPtr lParam);
+
+        public static String version = "1.0-snapshot";
+        public static String clipboard;
+        private bool ready = false;
+
+        MySettings settings = MySettings.Load();
+
+        IntPtr nextClipboardViewer;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            nextClipboardViewer = (IntPtr)SetClipboardViewer((int)
+                         this.Handle);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            versionLabel.Text = versionLabel.Text.Replace("%s", version);
+            shortSize.Value = settings.maxlength;
+            ready = true;
+        }
+
+        private void onClose(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            trayIcon.Visible = true;
+            trayIcon.ShowBalloonTip(1000);
+            e.Cancel = true;
+        }
+
+        private void onClick(object sender, EventArgs e)
+        {
+            this.Show();
+            trayIcon.Visible = false;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://stefanarts.net");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        void checkClipboard()
+        {
+            if(ready)
+            {
+                try
+                {
+                    IDataObject iData = new DataObject();
+                    iData = Clipboard.GetDataObject();
+
+                    if (iData.GetDataPresent(DataFormats.Text))
+                    {
+                        clipboard = (string)iData.GetData(DataFormats.Text);
+                        if (Uri.IsWellFormedUriString(clipboard, UriKind.Absolute))
+                        {
+                            if (clipboard.Length > settings.maxlength)
+                            {
+                                if (isOnline())
+                                {
+                                    System.Windows.Forms.Clipboard.SetText(shortUrlWithSLS(clipboard));
+                                }
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
+            }
+        }
+
+        private String shortUrlWithSLS(String url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string raw = client.DownloadString("https://s-ls.de/?url=" + url + "&linkhelper");
+                var link = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(raw);
+                return "https://s-ls.de/" + link.link.shortname;
+            }
+        }
+
+        protected override void
+          WndProc(ref System.Windows.Forms.Message m)
+        {
+            // defined in winuser.h
+            const int WM_DRAWCLIPBOARD = 0x308;
+            const int WM_CHANGECBCHAIN = 0x030D;
+
+            switch (m.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    checkClipboard();
+                    SendMessage(nextClipboardViewer, m.Msg, m.WParam,
+                                m.LParam);
+                    break;
+
+                case WM_CHANGECBCHAIN:
+                    if (m.WParam == nextClipboardViewer)
+                        nextClipboardViewer = m.LParam;
+                    else
+                        SendMessage(nextClipboardViewer, m.Msg, m.WParam,
+                                    m.LParam);
+                    break;
+
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+
+        public static bool isOnline()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://google.com/generate_204"))
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void shortSize_ValueChanged(object sender, EventArgs e)
+        {
+            settings.maxlength = (int)shortSize.Value;
+            settings.Save();
+        }
+    }
+
+    class MySettings : AppSettings<MySettings>
+    {
+        public int maxlength = 25;
+    }
+}
